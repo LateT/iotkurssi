bb9daa0bf05623daf2a3c539973fd86e9c24a1d1
Lauri Timonen <late.timonen@gmail.com>
2025-05-01 20:19:04 +0300

    update: update main_iotserver.yml to build from right paths and update index.js to serve react's pages in order to deploy whole project under one web app

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,24 @@
 require('dotenv').config();
-// console.log(process.env.TESTI);
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app's build directory
+const frontendPath = path.join(__dirname, '../frontend/build');
+app.use(express.static(frontendPath));
+
+// Api routes
+app.get("/test", (req, res) => {
+    return res.json({
+        error: false
+    })
+})
+
+// Catch all routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"))
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running...');
+});
