6dbb6cb1fd17c91c21f6e198d493faad59ded21f
Lauri Timonen <late.timonen@gmail.com>
2025-05-01 23:48:21 +0300

    another attempt at fixing automatic deployment

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('node:fs');
-const frontendPath = path.join(process.cwd(),"../frontend/build/")
-// Debugging
-fs.writeFile(__dirname + ".txt", frontendPath, err => {
-    if (err) {
-      console.error(err);
-    } else {
-      // file written successfully
-    }
-  });
 
 // Serve static files from the React app's build directory
-
+const frontendPath = path.join(__dirname,"../frontend/build/")
 app.use(express.static(frontendPath));
 
 // Api routes
@@ -29,6 +19,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"))
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
 });
